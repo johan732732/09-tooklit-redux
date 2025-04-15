b59a37a167c8f5c5b712ff12026795b0819c7c66
Johan Córdoba <johan732732@gmail.com>
2025-04-15 14:11:51 -0500

    pokemonslice

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+
+      <ul>
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+        <li>Hi</li>
+      </ul>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 
 import { store } from "./store";
+import { PokemonApp } from "./PokemonApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
+      {/* <App /> */}
     </Provider>
   </StrictMode>
 );
