d874602a18fb106d809033ca9bcfbf905e600acf
Johan Córdoba <johan732732@gmail.com>
2025-04-15 14:51:28 -0500

    Use axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,13 +1,17 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    const res = await fetch(
-      `https://pokeapi.co/api/v2/pokemon?offset=${page * 10}&limit=10`
+    // const res = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?offset=${page * 10}&limit=10`
+    // );
+    // const data = await res.json();
+    const { data } = await pokemonApi.get(
+      `pokemon?offset=${page * 10}&limit=10`
     );
-    const data = await res.json();
 
     dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
   };
